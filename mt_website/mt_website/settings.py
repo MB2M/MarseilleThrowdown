9fa36e89f5f7864043299fd8bf52aa682336bcde
Damien Sabouraud <damsbooster@hotmail.com>
2021-04-17 01:04:47 +0200

    update sponsor URL

--- mt_website/mt_website/settings.py
+++ mt_website/mt_website/settings.py
@@ -124,9 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 STATIC_ROOT = 'static'
 
 # Default primary key field type
