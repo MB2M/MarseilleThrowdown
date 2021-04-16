a9c3e023ffebf014802905cabcae9f8bbdad029b
Damien Sabouraud <damsbooster@hotmail.com>
2021-04-17 01:02:17 +0200

    move the templates folder

--- mt_website/mt_website/settings.py
+++ mt_website/mt_website/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'mt_website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['mt_website/templates/'],
+        'DIRS': ['templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,6 +127,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
