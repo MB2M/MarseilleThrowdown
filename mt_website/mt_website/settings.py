6226296891aea2ad022110e6e400980132335b7d
Damien Sabouraud <damsbooster@hotmail.com>
2021-04-18 20:45:08 +0200

    add contact us

--- mt_website/mt_website/settings.py
+++ mt_website/mt_website/settings.py
@@ -133,3 +133,9 @@ STATIC_ROOT = 'static'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local import *
+except ImportError:
+    print("In production")
+
--- mt_website/static_pages/views.py
+++ mt_website/static_pages/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import redirect, render
+from django.views.generic import TemplateView, RedirectView
+from django.views.generic.edit import FormMixin
+from .forms import ContactForm
 
 class Index(TemplateView):
     template_name = 'static_pages/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['contact_form'] = ContactForm()
+        return context
+
+class ContactEmail(RedirectView, FormMixin):
+
+    pattern_name = 'static_pages:index'
+    form_class = ContactForm
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            form.send_email()
+            print('mailing OK')
+            return self.get(request, *args, **kwargs)
+        else:
+            print('mailing error')
+            return self.get(request, *args, **kwargs)
